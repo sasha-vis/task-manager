b9c0df9de0ae9835bfb72e71b873bf9f5670f2b3
Aleksandr Vysotski <sasha.vysocki@gmail.com>
2025-06-20 21:16:19 +0300

    deploy: Добавил Docker
    Создал файлы для работы докера, настроил сборку

--- server/app.js
+++ server/app.js
@@ -2,24 +2,33 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-// import cors from 'cors';
 import routes from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-// app.use(
-// 	cors({
-// 		origin: 'http://localhost:5178',
-// 		credentials: true,
-// 	}),
-// );
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
+
+if (process.env.NODE_ENV === 'production') {
+	const publicPath = path.join(__dirname, 'public');
+	const indexFile = path.join(publicPath, 'index.html');
+
+	app.use(express.static(publicPath));
+
+	app.get('*', (req, res) => {
+		res.sendFile(indexFile);
+	});
+}
 
 mongoose
 	.connect(process.env.MONGO_URL)
